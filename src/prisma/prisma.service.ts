78487ebd28f8c4bb9e71cf7fcb01e2bc4768662f
Hiro <laciferin@gmail.com>
2023-07-07 22:40:48 +0530

    add: primsa with db url
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 4 minutes

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          //TODO: config module
+          url: 'postgresql://hiro:R_Z4ssy4hOAzHRXWB-6qNg@nest-js-5295.8nk.cockroachlabs.cloud:26257/bookmark-mgmt?sslmode=verify-full',
+        },
+      },
+    })
+  }
+}
