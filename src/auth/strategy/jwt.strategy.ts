7a1a20e40f978c8434b4c9adc76eaa99634dce7f
Hiro <laciferin@gmail.com>
2023-07-09 05:41:03 +0530

    added: guards
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 5 minutes

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -4,7 +4,7 @@ import { ExtractJwt, Strategy } from "passport-jwt"
 import { ConfigService } from "@nestjs/config"
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
   private jwtConstants: any
 
   constructor(config: ConfigService) {
