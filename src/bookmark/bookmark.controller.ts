79ac06f1aa6d3a9d2d543da4372fdedb3c27b8cc
Hiro <laciferin@gmail.com>
2023-07-09 10:04:53 +0530

    begin: bookmark
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 19 minutes

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from "@nestjs/common"
+import { Body, Controller, Delete, Get, Patch, UseGuards } from "@nestjs/common"
+import { JwtGuard } from "../auth/guard"
+import { BookmarkService } from "./bookmark.service"
+import { GetUser } from "../auth/decorator"
+import { EditBookmarkDto } from "./dto"
 
-@Controller("bookmark")
-export class BookmarkController {}
+@UseGuards(JwtGuard)
+@Controller("bookmarks")
+export class BookmarkController {
+  constructor(private bookmarkService: BookmarkService) {}
+
+  @Get()
+  getBookmarks(@GetUser("userId") userId: number) {
+    return this.bookmarkService.getAll(userId)
+  }
+
+  @Get(":bookmarkId") //TODO: params
+  getBookmark(bookmarkId: number) {
+    return this.bookmarkService.get(bookmarkId)
+  }
+
+  @Patch(":bookmarkId") //TODO: params
+  editBookmark(bookmarkId: number, @Body() dto: EditBookmarkDto) {
+    return this.bookmarkService.edit(bookmarkId, dto)
+  }
+
+  @Delete(":bookmarkId") //TODO: params
+  deleteBookmark(bookmarkId: number) {
+    return this.bookmarkService.delete(bookmarkId)
+  }
+}
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from "@nestjs/common"
+import { PrismaService } from "../prisma/prisma.service"
+import { EditBookmarkDto } from "./dto"
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+  constructor(private prisma: PrismaService) {}
+
+  getAll(userId: number) {}
+
+  get(id: number) {}
+
+  edit(id: number, dto: EditBookmarkDto) {}
+
+  delete(id: number) {}
+}
