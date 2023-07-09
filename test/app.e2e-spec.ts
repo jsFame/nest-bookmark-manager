aa7042eb683ac64abd01bd1ccf839e8ddb75cb08
Hiro <laciferin@gmail.com>
2023-07-09 06:43:46 +0530

    teardown : db
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 9 minutes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { Test } from "@nestjs/testing"
 import { AppModule } from "../src/app.module"
 import { INestApplication, ValidationPipe } from "@nestjs/common"
+import { PrismaService } from "../src/prisma/prisma.service"
 
 describe("App e2e", () => {
   let app: INestApplication
+  let prisma: PrismaService
   beforeAll(async () => {
     const ModuleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -11,6 +13,8 @@ describe("App e2e", () => {
     app = ModuleRef.createNestApplication()
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
     await app.init()
+    prisma = app.get(PrismaService)
+    await prisma.cleanDb()
   })
   it.todo("should pass")
 
