5358f3bcae149af750231986edf6c1c590c5ee08
Hiro <laciferin@gmail.com>
2023-07-09 10:06:20 +0530

    fix: deps nest
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 1 minute

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common"
 import { BookmarkController } from "./bookmark.controller"
 import { BookmarkService } from "./bookmark.service"
+import { AuthModule } from "../auth/auth.module"
+import { PrismaModule } from "../prisma/prisma.module"
 
 @Module({
+  imports: [AuthModule, PrismaModule],
   controllers: [BookmarkController],
   providers: [BookmarkService],
 })
