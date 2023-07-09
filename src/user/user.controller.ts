ec0aafd7bbf1ae9bfd69aee7a662233961a8c87c
Hiro <laciferin@gmail.com>
2023-07-09 07:37:18 +0530

    story: edit user
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 12 minutes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, Patch, UseGuards } from "@nestjs/common"
+import { Body, Controller, Get, Patch, UseGuards } from "@nestjs/common"
 import { JwtGuard } from "../auth/guard"
 import { GetUser } from "../auth/decorator"
 import { User } from "@prisma/client"
+import { EditUserDto } from "./dto"
+import { UserService } from "./user.service"
 
 @Controller("users")
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @UseGuards(JwtGuard)
   @Get("me")
   getMe(@GetUser() user: User) {
@@ -12,5 +16,9 @@ export class UserController {
   }
 
   @Patch()
-  editUser() {}
+  editUser(@GetUser("id") userId: number, @Body() dto: EditUserDto) {
+    //needs service
+    // we will get something in api request due to whitelist:true
+    return this.userService.editUser(userId, dto)
+  }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,11 @@
 import { Module } from "@nestjs/common"
 import { UserController } from "./user.controller"
+import { UserService } from "./user.service"
+import { PrismaService } from "../prisma/prisma.service"
 
 @Module({
+  imports: [PrismaService],
   controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
